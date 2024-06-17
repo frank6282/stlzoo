439e3499d290108c0027e513aba2b6383cdde854
frank6282 <frankdil92@gmail.com>
2024-06-17 08:24:54 -0500

    rewrote species_list

--- stlzoo_proj/settings.py
+++ stlzoo_proj/settings.py
@@ -21,7 +21,7 @@ MESSAGE_TAGS = {messages.ERROR: "danger"}
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stlzoo.onrender.com"]
 
