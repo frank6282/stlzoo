30708645bcbc2db11a07db56bceb4e5ad53c683c
frank6282 <frankdil92@gmail.com>
2024-08-04 23:48:30 -0500

    changed allowed host

--- stlzooproj/settings.py
+++ stlzooproj/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://stlzoo-52tz.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stlzoo-52tz.onrender.com"]
 
 
 # Application definition
