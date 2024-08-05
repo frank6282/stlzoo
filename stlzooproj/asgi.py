ba5b2592832a2587c4b6a0a81db5a3fae0fa53c6
frank6282 <frankdil92@gmail.com>
2024-08-04 23:44:04 -0500

    changed project name

--- stlzooproj/asgi.py
+++ stlzooproj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stlzoo_proj project.
+ASGI config for stlzooproj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stlzoo_proj.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stlzooproj.settings")
 
 application = get_asgi_application()
